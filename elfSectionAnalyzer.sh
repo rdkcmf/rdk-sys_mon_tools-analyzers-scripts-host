bacccd17662376484613ab66f63a5ef1b872ed59
Sergei Danilchuk <sergei_danilchuk@cable.comcast.com>
2017-06-26 16:05:29 -0400

    RDKSYSINTSW-3527 : Implement support to analyze code/data sections of ELFs
    Reason for change: Implement support to analyze various ELF data sections -
    initialized/uninitialized data (.data/.bss), read-only data (.rodata) and
    read-only dynamically realocatable data (.data.rel.ro) for not-stripped
    ELF binaries as well as code (.text) section.
    Test Procedure: Execute elfSectionAnalyzer.sh on an unstripped ELF binary
    to analyze various type of code/data sections. The PATH enviromental
    variable must be set to point to platform tools.
    Risks: None.
    Signed-off-by: Sergei Danilchuk <sergei_danilchuk@cable.comcast.com>
    
    Change-Id: I9f462d5c2ca869f2c0df40e5281f268ef073aa44

--- elfSectionAnalyzer.sh
+++ elfSectionAnalyzer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 
+# Variables:
+ODDSFILTER='F \.text'$'\t'"\|"'O .text'$'\t'"\|"'O \.bss'$'\t'"\|"'O \.data'$'\t'"\|"'O \.rodata'$'\t'"\|"'O .data.rel.ro'$'\t'
+
 # Function: usage
 function usage()
 {
@@ -9,10 +12,23 @@ function usage()
 	echo "$name# -e    : an ELF object"
 	echo "$name# -od   : an objdump to use instead of default: {armeb-rdk-linux-uclibceabi-objdump | mipsel-linux-objdump | i686-cm-linux-objdump}"
 	echo "$name# -o    : an output file base name"
-	echo "$name# -s    : add source code locations to output"
 	echo "$name# -V    : validate produced data"
 	echo "$name# -F    : produce function file offsets - not implemented yet"
 	echo "$name# -h    : display this help and exit"
+}
+
+# Function: eshLog
+# $1: eshFile	- an elf section file
+# $2: eshFColumn- a column # describing object sizes
+# $3: eshLog	- log file
+function eshLog()
+{
+	if [ -s $1 ]; then
+		eshDescr=${1#$elf.}
+		awk -v eshFile="$1" -v eshSize="$2" -v eshDescr="$eshDescr" 'BEGIN {FS="\t"}; {total += $eshSize} END { printf "%-14s = %6d objects : %9d B / %8.2f KB / %5.2f MB : %s\n", eshDescr, NR, total, total/1024, total/(1024*1024), eshFile}' "$1" | tee -a $3
+	else
+		printf "%-14s = %6d objects : %9d B / %8.2f KB / %5.2f MB : %s\n" "$eshDescr" 0 0 0 0 "$1" | tee -a $3
+	fi
 }
 
 # Main:
@@ -28,7 +44,6 @@ of=
 elf=
 funcFO=
 objdump=
-source=
 validation=
 while [ "$1" != "" ]; do
 	case $1 in
@@ -42,8 +57,6 @@ while [ "$1" != "" ]; do
 				objdump=$1
 				;;
 		-V )		validation="y"
-				;;
-		-s )		source=y
 				;;
 		-F )   shift
 				funcFO=y
@@ -90,44 +103,75 @@ fi
 
 [ -z "$of" ] && of=${elf}
 
-echo "ELF = ${elf}" | tee -a ${name}.log
+readelf -S ${elf} > ${of}.readelf-S
+elfSymbols=$(grep " .debug_\| .pdr\| .comment\| .symtab\| .strtab" ${of}.readelf-S)
+if [ -z "$elfSymbols" ]; then
+	elfSymbols="stripped"
+else
+	elfSymbols="not stripped : "$(printf "%s\n" "${elfSymbols}" | tr -s '[]' ' ' | tr -s ' ' | cut -d ' ' -f3 | sed 's/.debug_.*/.dbg/g' | sort | uniq | tr -d '\n' | awk '{print $0}')
+fi
+
+echo "elf = ${elf}" : "${elfSymbols}" | tee -a ${name}.log
 echo "out = ${of}" | tee -a ${name}.log
 echo "od  = ${objdump}" | tee -a ${name}.log
 
 # create "start/end of executable sections" file
-readelf -S "$elf" | grep " AX " | tr -s '[]' ' ' | tr -s ' ' | cut -d ' ' -f3,5,7 | awk '{printf "0x%s\t%09d\t%s\n", $2, strtonum("0x"$3), $1}' > ${of}.ax-secs
+grep " AX " ${of}.readelf-S | tr -s '[]' ' ' | tr -s ' ' | cut -d ' ' -f3,5,7 | awk '{printf "0x%s\t%09d\t%s\n", $2, strtonum("0x"$3), $1}' > ${of}.ax-secs
 endst=$(tail -1 ${of}.ax-secs | cut -f1)
 endsi=$(tail -1 ${of}.ax-secs | cut -f2 | awk '{printf "0x%x\n", $1}')
 start=$(head -1 ${of}.ax-secs | cut -f1)
 end=$((endst + endsi - 1))
 
-# create function map file: 1c - start address; 2c - length; 3c - function file offset; 4c - function name
-_err_=$?
-${objdump} -dC "$elf" | grep "^[[:xdigit:]]\{8\}" | awk '{printf "0x%s\n", $0}' > ${of}.ax.tmp
-if [ $_err_ != 0 ]; then
-	echo "$name: Error=$_err_ executing ${objdump} ${elf}. Exit." | tee -a ${name}.log
-	exit 5
-fi
-pFuncAddr=
-pFuncAttr=
-cat /dev/null > ${of}.ax
-while read -r cFuncAddr cFuncAttr
-do
-	[ ! -z "$pFuncAddr" ] && printf "0x%08x\t%09d\t%s\n" $pFuncAddr $((cFuncAddr-pFuncAddr)) "$pFuncName" >> ${of}.ax
-	pFuncAddr=$cFuncAddr
-	pFuncName=$cFuncAttr
-done < ${of}.ax.tmp
-lastFunc=$(tail -1 ${of}.ax.tmp)
-printf "%s\t%09d\t%s\n" $(echo $lastFunc | cut -d ' ' -f1) $endsi $(echo $lastFunc | cut -d ' ' -f2-) >> ${of}.ax
-if [ ! -z "$source" ]; then
-	if [ ! -z "$(echo $isElf | grep "not stripped$")" ]; then
-		maxExt=$(cut -f3 ${of}.ax | wc -L | cut -f1)
-		printf "%-10s\t%-9s\t%-*s\t%s\n" "Address" "Size" $maxExt "Function Name" "Function source location"> ${of}.ax.source
-		cut -f1 ${of}.ax | addr2line -Cp -e ${elf} | paste ${of}.ax - | awk -v mL=$maxExt 'BEGIN {FS="\t"}; {printf "%s\t%s\t%-*s\t%s\n", $1, $2, mL, $3, $4}' >> ${of}.ax.source
-	else
-		echo "$name# WARN : ELF ${elf} is stripped, no symbols"
+notStripped=$(echo $isElf | grep "not stripped$")
+if [ -z "$notStripped" ]; then
+	# create AX section map file: 1c - start address; 2c - length; (3c - function file offset) not implemented; 4c - function name
+	_err_=$?
+	${objdump} -dC "$elf" | grep "^[[:xdigit:]]\{8\}" | sed 's/ </ /;s/>:$//' | awk '{printf "0x%s\n", $0}' > ${of}.ax.tmp
+	if [ $_err_ != 0 ]; then
+		echo "$name: Error=$_err_ executing ${objdump} ${elf}. Exit." | tee -a ${name}.log
+		exit 5
 	fi
+	pFuncAddr=
+	pFuncAttr=
+	cat /dev/null > ${of}.ax
+	while read -r cFuncAddr cFuncAttr
+	do
+		[ ! -z "$pFuncAddr" ] && printf "0x%08x\t%09d\t%s\n" $pFuncAddr $((cFuncAddr-pFuncAddr)) "$pFuncName" >> ${of}.ax
+		pFuncAddr=$cFuncAddr
+		pFuncName=$cFuncAttr
+	done < ${of}.ax.tmp
+	lastFunc=$(tail -1 ${of}.ax.tmp)
+	printf "%s\t%09d\t%s\n" $(echo $lastFunc | cut -d ' ' -f1) $endsi $(echo $lastFunc | cut -d ' ' -f2-) >> ${of}.ax
+
+	# Cleanup
+	rm -f ${of}.ax.tmp
+else
+	${objdump} -tC "$elf" | sort -u | grep "$ODDSFILTER" | tr -s ' ' | cut -d ' ' -f1,3- | sed 's/F ./F/;s/O ./O/;s/\t/ /;s/ /\t/;s/ /\t/;s/ /\t/' | awk -F'\t' '{printf "%-10s\t0x%s\t%09d\t%s\n", $2, $1, strtonum("0x"$3), $4}' > ${of}.odds
+	printf '' | tee ${of}.Ftext ${of}.Otext ${of}.Odata ${of}.Orodata ${of}.Odata.rel.ro ${of}.Obss
+	while IFS=$'\t' read section therest; do
+		echo "$therest" >> ${of}.${section}
+	done < ${of}.odds
+	cat ${of}.Ftext <(grep -v "\.text" ${of}.ax-secs) | sort -k1,1 -o ${of}.ax
+
+	# calculate a number of section objects and their sizes
+	for file in ${of}.Ftext ${of}.Otext ${of}.Odata ${of}.Orodata ${of}.Odata.rel.ro ${of}.Obss; do
+		if [ -s ${file} ]; then
+			eshLog ${file} 2 ${name}.log
+		fi
+	done
+
+	# create a formatted ${of}.ax.source file
+	maxExt=$(cut -f3 ${of}.ax | wc -L | cut -f1)
+	printf "%-10s\t%-9s\t%-*s\t%s\n" "Address" "Size" $maxExt "Function Name" "Function source location"> ${of}.ax.source
+	cut -f1 ${of}.ax | addr2line -Cp -e ${elf} | paste ${of}.ax - | awk -v mL=$maxExt 'BEGIN {FS="\t"}; {printf "%s\t%s\t%-*s\t%s\n", $1, $2, mL, $3, $4}' >> ${of}.ax.source
+
+	#Cleanup
+	rm -f ${of}.odds
+	for file in ${of}.Ftext ${of}.Otext ${of}.Odata ${of}.Orodata ${of}.Odata.rel.ro ${of}.Obss; do
+		[ ! -s ${file} ] && rm ${file} || sed -i "1i$ESH_HEADER_OBJ_1" ${file}
+	done
 fi
+
 sort -rnk2 ${of}.ax -o ${of}.ax.rnk2
 
 totalAXSecsSize=$(awk '{total += $2} END { printf "%d\n", total} ' ${of}.ax-secs)
@@ -136,11 +180,11 @@ if [ "$validation" == "y" ]; then
 	totalAXSecsSizeFile=$(awk '{total += $2} END { printf "%d\n", total} ' ${of}.ax)
 	totalAXSecsSizeRnk2File=$(awk '{total += $2} END { printf "%d\n", total} ' ${of}.ax.rnk2)
 	if [ "$totalAXSecsSpace" -ne "$totalAXSecsSizeFile" ]; then
-		echo "$name# WARN : validation failed for ${of}.ax : file total size = $totalAXSecsSizeFile : total AX sections space = $totalAXSecsSpace : diff = $((totalAXSecsSizeFile-totalAXSecsSpace))" | tee -a ${name}.log
+		echo "$name# total AX sections size/space mismatch for ${of}.ax : size = $totalAXSecsSizeFile : space = $totalAXSecsSpace : size - space = $((totalAXSecsSizeFile-totalAXSecsSpace))" | tee -a ${name}.log
 	elif [ "$totalAXSecsSpace" -ne "$totalAXSecsSizeRnk2File" ]; then
-		echo "$name# WARN : validation failed for ${of}.ax.rnk2: file total size = $totalAXSecsSizeRnk2File : total AX sections space = $totalAXSecsSpace : diff = $((totalAXSecsSizeFile-totalAXSecsSpace))" | tee -a ${name}.log
+		echo "$name# total AX sections size/space mismatch for ${of}.ax.rnk2: size = $totalAXSecsSizeRnk2File : space = $totalAXSecsSpace : size - space = $((totalAXSecsSizeFile-totalAXSecsSpace))" | tee -a ${name}.log
 	else
-		echo "$name# : validation success : total AX secs space = $totalAXSecsSpace : ${of}.ax ${of}.ax.rnk2" | tee -a ${name}.log
+		echo "$name# : validation success : total AX secs size/space = $totalAXSecsSpace : ${of}.ax ${of}.ax.rnk2" | tee -a ${name}.log
 	fi
 fi
 
@@ -152,5 +196,3 @@ sed -i "1i$ESH_HEADER_TEXT_1" ${of}.ax.rnk2
 sed -i "1i$ESH_HEADER_AXSECS" ${of}.ax-secs
 printf "%10s%09d/%09d\t%s\n" " " $totalAXSecsSpace $totalAXSecsSize "Total space/size" >> ${of}.ax-secs
 
-# Cleanup
-rm ${of}.ax.tmp
